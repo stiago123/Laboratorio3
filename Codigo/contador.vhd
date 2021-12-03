0b6367299a4956a8152d4623290bfb003daeda11
Santiago Mariño <smarinoj@unal.edu.co>
2021-12-03 01:33:51 -0500

    Avance con gabo ffjk, cont, cont60

--- Codigo/contador.vhd
+++ Codigo/contador.vhd
@@ -7,7 +7,7 @@ use ieee.std_logic_unsigned.all;
 entity contador is
 port (
 		rst: in std_logic;
-		clk: in std_logic;
+		fin: in std_logic;
 		en:  in std_logic;
 		
 		num: out std_logic_vector(5 downto 0)
@@ -18,9 +18,12 @@ architecture contadorisacion of contador is
 
 component ffJK
 	Port ( 
+		
 		J:in std_logic;
 		K:in std_logic;
 		clk:in std_logic;
+		clear:in std_logic;
+		en:	in std_logic;
 		
 		Q: out std_logic
 		);
@@ -31,12 +34,12 @@ component ffJK
 begin
 reset <= not rst;
 
-	ffjk0: ffJK port map('1' xor (reset and not q0) ,'1' xor (reset and not q0), clk and en, q0);
-	ffjk1: ffJK port map(q0 or (reset and q1),q0 or (reset and q1) , clk and en, q1);
-	ffjk2: ffJK port map((q0 and q1) or (q2 and q3 and q4 and q5) or (reset and q2),(q0 and q1) or (q2 and q3 and q4 and q5) or (reset and q2) , clk and en, q2);
-	ffjk3: ffJK port map((q0 and q1 and q2) or (q2 and q3 and q4 and q5) or (reset and q3),(q0 and q1 and q2) or (q2 and q3 and q4 and q5) or (reset and q3), clk and en, q3);
-	ffjk4: ffJK port map((q0 and q1 and q2 and q3) or (q2 and q3 and q4 and q5) or (reset and q4),(q0 and q1 and q2 and q3) or (q2 and q3 and q4 and q5) or (reset and q4), clk and en, q4);
-	ffjk5: ffJK port map((q0 and q1 and q2 and q3 and q4) or (q2 and q3 and q4 and q5) or (reset and q5),(q0 and q1 and q2 and q3 and q4) or (q2 and q3 and q4 and q5) or (reset and q5), clk and en, q5);
+	ffjk0: ffJK port map('1' xor (reset and not q0) ,'1' xor (reset and not q0), fin, reset, en, q0);
+	ffjk1: ffJK port map(q0 or (reset and q1),q0 or (reset and q1) , fin, reset, en, q1);
+	ffjk2: ffJK port map((q0 and q1) or (q2 and q3 and q4 and q5) or (reset and q2),(q0 and q1) or (q2 and q3 and q4 and q5) or (reset and q2) ,  fin, reset, en, q2);
+	ffjk3: ffJK port map((q0 and q1 and q2) or (q2 and q3 and q4 and q5) or (reset and q3),(q0 and q1 and q2) or (q2 and q3 and q4 and q5) or (reset and q3), fin, reset, en, q3);
+	ffjk4: ffJK port map((q0 and q1 and q2 and q3) or (q2 and q3 and q4 and q5) or (reset and q4),(q0 and q1 and q2 and q3) or (q2 and q3 and q4 and q5) or (reset and q4), fin, reset, en, q4);
+	ffjk5: ffJK port map((q0 and q1 and q2 and q3 and q4) or (q2 and q3 and q4 and q5) or (reset and q5),(q0 and q1 and q2 and q3 and q4) or (q2 and q3 and q4 and q5) or (reset and q5), fin, reset, en, q5);
 
 
 	
